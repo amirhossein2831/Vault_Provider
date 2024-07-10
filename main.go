da547b2d7d42b6a391018d68e817a90c55e2c274
amirhossein <amirmemool12@gmail.com>
2024-07-10 18:27:05 +0330

    :hammer: chore: read sensetive var from .env

--- main.go
+++ main.go
@@ -3,22 +3,37 @@ package main
 import (
 	"github.com/amirhossein2831/Vault_Provider/src/handler"
 	"github.com/amirhossein2831/Vault_Provider/src/pkg"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/go-chi/chi/v5"
 	"github.com/hashicorp/vault/api"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Load env var
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Init  env var
+	vaultAdd := os.Getenv("VAULT_ADDRESS")
+	vaultToken := os.Getenv("VAULT_TOKEN")
+	vaultPath := os.Getenv("VAULT_PATH")
+	appPort := os.Getenv("APP_PORT")
+
 	// Initial the vault
 	pkg.NewVault(&api.Config{
-		Address: "http://127.0.0.1:8200",
+		Address: vaultAdd,
 	})
 
 	// Read the variable
-	err := pkg.GetVault().SetToken("root").ReadVault("secret/data/app")
+	err = pkg.GetVault().SetToken(vaultToken).ReadVault(vaultPath)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 
 	// Initial the router
@@ -30,11 +45,11 @@ func main() {
 
 	// Initial the server
 	go func() {
-		err := http.ListenAndServe(":2000", router)
+		err := http.ListenAndServe(spew.Sprintf(":%v", appPort), router)
 		if err != nil {
 			log.Fatal("failed to start server")
 		}
 	}()
-	println("server started successfully on port 2000")
+	println("server started successfully on port " + appPort)
 	select {}
 }
