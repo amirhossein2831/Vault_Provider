c22994f4ce6c1010f99ac52d7059dadfba4b9ef1
amirhossein <amirmemool12@gmail.com>
2024-07-10 18:18:16 +0330

    :hammer: chore: bootstrap vault and router

--- main.go
+++ main.go
@@ -1,56 +1,40 @@
 package main
 
 import (
-	"encoding/json"
+	"github.com/amirhossein2831/Vault_Provider/src/handler"
+	"github.com/amirhossein2831/Vault_Provider/src/pkg"
 	"github.com/go-chi/chi/v5"
+	"github.com/hashicorp/vault/api"
 	"log"
 	"net/http"
 )
 
-type User struct {
-	Name           string `json:"name"`
-	SecretPassword string `json:"password"`
-	NationalId     string `json:"nationalId"`
-}
-
-func Users() []User {
-	return []User{
-		{Name: "amir", SecretPassword: "Password", NationalId: "12341234"},
-		{Name: "ali", SecretPassword: "myPass", NationalId: "12121212"},
-		{Name: "ali", SecretPassword: "varySecretPass", NationalId: "11111111"},
-	}
-}
-
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	users := Users()
-
-	marshal, err := json.Marshal(users)
-	if err != nil {
-		log.Fatal("failed to marshal users")
-	}
-	_, err = w.Write(marshal)
-	if err != nil {
-		log.Fatal("failed to write response")
-	}
-}
 func main() {
-	router := chi.NewRouter()
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("welcome"))
-		if err != nil {
-			log.Fatal("failed to write response")
-		}
+	// Initial the vault
+	pkg.NewVault(&api.Config{
+		Address: "http://127.0.0.1:8200",
 	})
 
-	router.Get("/posts", GetUser)
+	// Read the variable
+	err := pkg.GetVault().SetToken("root").ReadVault("secret/data/app")
+	if err != nil {
+		return
+	}
 
+	// Initial the router
+	router := chi.NewRouter()
+
+	// Initial the route
+	router.Get("/users", handler.GetUsers)
+	router.Get("/users/secret", handler.GetUser)
+
+	// Initial the server
 	go func() {
 		err := http.ListenAndServe(":2000", router)
 		if err != nil {
 			log.Fatal("failed to start server")
 		}
 	}()
-
 	println("server started successfully on port 2000")
 	select {}
 }
